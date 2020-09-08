515a807c8a52b554f228ed5b478450cfba4369a7
hosmanoglu <huseyin.osmanogluu.61@gmail.com>
2020-09-08 20:19:35 +0300

    run chrome before

--- test/index.test.js
+++ test/index.test.js
@@ -9,18 +9,15 @@ const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 var ChromeOptions = new chrome.Options()
 ChromeOptions.headless()
- .addArguments("start-maximized")
- .addArguments("disable-infobars")
- .addArguments("--disable-extensions")
- .addArguments("--disable-gpu")
- .addArguments("--disable-dev-shm-usage")
- .addArguments("--no-sandbox")
- 
+  .addArguments("start-maximized")
+  .addArguments("disable-infobars")
+  .addArguments("--disable-extensions")
+  .addArguments("--disable-gpu")
+  .addArguments("--disable-dev-shm-usage")
+  .addArguments("--no-sandbox")
 
-const driver = new webdriver.Builder().forBrowser('chrome')
-//.setChromeOptions(new chrome.Options().headless())
-.setChromeOptions(ChromeOptions)
-.build()
+
+var driver;
 
 
 
@@ -36,68 +33,76 @@ chai.use(chaiHttp);
 const server = require("../main");
 
 describe('src', function () {
-    it('index.css', done => {
-        chai.request(server)
-            .get("/src/index.css")
-            .set('content-type', 'text/css')
-            .end((error, response) => {
-                response.should.have.status(200);
-                done()
+  it('index.css', done => {
+    chai.request(server)
+      .get("/src/index.css")
+      .set('content-type', 'text/css')
+      .end((error, response) => {
+        response.should.have.status(200);
+        done()
 
-            })
-    });
-    it('index.js', done => {
-        chai.request(server)
-            .get("/src/index.css")
-            .set('content-type', 'text/javascript')
-            .end((error, response) => {
-                response.should.have.status(200);
-                done()
-            })
-    });
-    it('jquery.', done => {
-        chai.request(server)
-            .get("/src/index.css")
-            .set('content-type', 'src/jquery-3.5.1.min.js')
-            .end((error, response) => {
-                response.should.have.status(200);
-                console.log("jquery ok")
-                done()
-            })
-    });
+      })
+  });
+  it('index.js', done => {
+    chai.request(server)
+      .get("/src/index.css")
+      .set('content-type', 'text/javascript')
+      .end((error, response) => {
+        response.should.have.status(200);
+        done()
+      })
+  });
+  it('jquery.', done => {
+    chai.request(server)
+      .get("/src/index.css")
+      .set('content-type', 'src/jquery-3.5.1.min.js')
+      .end((error, response) => {
+        response.should.have.status(200);
+        console.log("jquery ok")
+        done()
+      })
+  });
 })
 
 
 
 
 describe('ekle check sil', () => {
-  
-  let testText="denemewasasqqwsswewwweqw"
-  it('ekle check sil',done   => {
-    driver.get('http://localhost:8080').then(function(res) {
+
+  before(done => {
+    driver = new webdriver.Builder().forBrowser('chrome')
+      //.setChromeOptions(new chrome.Options().headless())
+      .setChromeOptions(ChromeOptions)
+      .build()
+      then(done())
+  });
+
+  let testText = "denemewasasqqwsswewwweqw"
+  it('ekle check sil', done => {
+    driver.get('http://localhost:8080').then(function (res) {
       driver
         .findElement(By.id('add-text'))
-        .sendKeys(testText)
-        .then(() => driver.findElement(By.id('add-button')).click())
+        .sendKeys(testText)// type text
+        .then(() => driver.findElement(By.id('add-button')).click()) //click add button
         .then(() => {
           driver.wait(until.elementLocated(By.css('[id="list"]>div'))).then(async () => {
-        
-            var jobText= await driver.findElement(By.css('[id="list"]>div>[value="'+testText+'"]')).getAttribute("value")
-            expect(jobText).to.equal(testText);
-            var checkbox= await driver.findElement(By.css('[id="list"]>div>[type="checkbox"]')).getAttribute("checked")
-            expect(checkbox).to.be.a('null');
-            var button= await driver.findElement(By.css('[id="list"]>div>[type="button"]')).getAttribute("onclick")
-            expect(button).to.include('del')
 
-            await driver.findElement(By.css('[id="list"]>div>[value="'+testText+'"]+[type="checkbox"]')).click()
-            var checkboxVal= await driver.findElement(By.css('[id="list"]>div>[value="'+testText+'"]+[type="checkbox"]')).getAttribute("checked")
-            expect(checkboxVal).to.equal("true");
-            
-            await driver.findElement(By.css('[id="list"]>div>[value="'+testText+'"]+[type="checkbox"]+[type="button"]')).click()
-            
-            var list= await driver.findElement(By.css('[id="list"]')).getAttribute("innerHTML")
-            expect(button).to.not.include(testText)
-            
+            var jobText = await driver.findElement(By.css('[id="list"]>div>[value="' + testText + '"]')).getAttribute("value")
+            expect(jobText).to.equal(testText);//find typed text in list
+            var checkbox = await driver.findElement(By.css('[id="list"]>div>[type="checkbox"]')).getAttribute("checked")
+            expect(checkbox).to.be.a('null');// typed text line not checked 
+            var button = await driver.findElement(By.css('[id="list"]>div>[type="button"]')).getAttribute("onclick")
+            expect(button).to.include('del')// del button exist
+
+            await driver.findElement(By.css('[id="list"]>div>[value="' + testText + '"]+[type="checkbox"]')).click()
+            var checkboxVal = await driver.findElement(By.css('[id="list"]>div>[value="' + testText + '"]+[type="checkbox"]')).getAttribute("checked")
+            expect(checkboxVal).to.equal("true");// check checkbox
+
+            await driver.findElement(By.css('[id="list"]>div>[value="' + testText + '"]+[type="checkbox"]+[type="button"]')).click()
+
+            var list = await driver.findElement(By.css('[id="list"]')).getAttribute("innerHTML")
+            expect(button).to.not.include(testText)// click delete button then check list not include the test
+
             await driver.quit();
             done();
           });
